a1cb760b5df6c6c3435abc5bcc1cd76d4b0312a1
asraful7711 <asraful7711@gmail.com>
2022-10-17 19:21:21 +0600

    show data on modal and also change modal data if again clicked another button

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const allNewsCategories = async () => {
     }
 }
 allNewsCategories()
-
+//display all categories
 const displayCategories = categories => {
     // console.log(categories)
     const categoriesSection = document.getElementById('categories-section');
@@ -27,19 +27,21 @@ const displayCategories = categories => {
 }
 
 
-const allNews = async (newsid) => {
-    console.log(newsid);
+const allNews = async (catagoryId) => {
+    console.log(catagoryId);
     try {
-        url = `https://openapi.programming-hero.com/api/news/category/${newsid}`
+        url = `https://openapi.programming-hero.com/api/news/category/${catagoryId}`
         res = await fetch(url);
         data = await res.json();
         displayAllNews(data.data);
+
     }
     catch (error) {
         console.log(error);
     }
 }
 
+// display all news in card
 const displayAllNews = (allNews) => {
     // console.log(allNews)
     const newsField = document.getElementById('news-field');
@@ -67,7 +69,8 @@ const displayAllNews = (allNews) => {
                         <p><i class="fa-solid fa-eye"></i> ${news?.total_view ? news.total_view : 'NO VIEWS'} </p>
                     </div>
                     <div class=" justify-end">
-                        <button class="btn btn-active"><i class="fa-solid fa-arrow-right"></i></button>
+                        
+                        <label onclick="individualNews('${news._id}')" for="my-modal-3" class="btn btn btn-active"><i class="fa-solid fa-arrow-right"></i></label>
                     </div>
                     
                 </div>
@@ -79,4 +82,55 @@ const displayAllNews = (allNews) => {
         newsField.appendChild(div);
     })
 
+
 }
+//view all data in a modal
+const individualNews = async (newsId) => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/${newsId}`
+        const res = await fetch(url);
+        const data = await res.json();
+        viewFullNews(data.data[0])
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+const modalDiv = document.createElement('div');
+const viewFullNews = (singleData) => {
+    console.log(singleData)
+    const modalSection = document.getElementById('modal-section')
+    // modalSection.innerHTML = '';
+    modalDiv.classList.add('modal')
+    modalDiv.innerHTML = `
+                <div class="modal-box w-full relative">
+                <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <div class="card  'w-3/4'mg  alt="Shoes>
+                    <figure><img  src="${singleData.image_url}" alt="Album" /></figure>
+                    <div class="card-body">
+                            <h2 class="card-title text-xl font-bold mb-7">${singleData.title}</h2>
+                            <p class="text-lg text-gray-500"> ${singleData?.details}</p>
+                        <div class="flex justify-between">
+                            <div class="flex items-center">
+                                <img class="w-14 rounded-full mr-3" src="${singleData.author.img}" />
+                                <div> 
+                                    <p class="font-medium text-gray-500"> ${singleData.author?.name ? singleData.author?.name : 'NO DATA'}</p>
+                                    <p class="font-medium text-gray-500"> ${singleData.author?.published_date ? singleData.author?.published_date : 'NO date'}</p>
+
+                                </div>
+                            </div>
+                            <div class="flex items-center font-medium text-gray-500"> 
+                                <p><i class="fa-solid fa-eye"></i> ${singleData?.total_view ? singleData.total_view : 'NO VIEWS'} </p>
+                            </div>       
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    `
+    modalSection.appendChild(modalDiv)
+}
+
