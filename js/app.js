0608989b2021d7f5c6028dbb1430f53f199cda98
asraful7711 <asraful7711@gmail.com>
2022-10-17 13:55:20 +0600

    dinamicaly categories showed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+const allNewsCategories = async () => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/categories`;
+        const res = await fetch(url);
+        const data = await res.json();
+
+        displayCategories(data.data.news_category);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayCategories = categories => {
+    // console.log(categories)
+    const categoriesSection = document.getElementById('categories-section');
+
+    categories.forEach(category => {
+        console.log(category)
+        const li = document.createElement('li');
+        li.classList.add('mx-3', 'text-slate-500', 'font-medium',)
+        li.setAttribute("id", ``)
+        li.innerText = `${category.category_name}`
+        categoriesSection.appendChild(li)
+
+    });
+}
+allNewsCategories()
